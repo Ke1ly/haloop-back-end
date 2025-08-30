8814ecbd7e23b15c1e269e9aa1e33f9ff6018f3b
Ke1ly <o91260210@gmail.com>
2025-08-30 21:48:22 +0800

    Modify OpenSearch connection method

--- backend/src/config/esClient.ts
+++ backend/src/config/esClient.ts
@@ -1,8 +1,23 @@
 import { Client } from "@opensearch-project/opensearch";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
+import { AwsSigv4Signer } from "@opensearch-project/opensearch/aws";
 
 let esClient: Client;
 if (process.env.NODE_ENV === "production") {
   esClient = new Client({
+    ...AwsSigv4Signer({
+      region: process.env.AWS_REGION!,
+      service: "es",
+      getCredentials: async () => {
+        const provider = defaultProvider();
+        const creds = await provider();
+        return {
+          accessKeyId: creds.accessKeyId,
+          secretAccessKey: creds.secretAccessKey,
+          sessionToken: creds.sessionToken,
+        };
+      },
+    }),
     node: process.env.OPENSEARCH_URL!,
     requestTimeout: 30000,
     //     auth: {
